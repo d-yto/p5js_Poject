13a87ca2e921a8e698824e68fc59369c67c78435
Aidan Wallace <49172003@edu.cbe.ab.ca>
2026-05-19 12:33:24 -0600

    i started working on BT but forgot i only had likle 2 minutes

--- bt.js
+++ bt.js
@@ -1,8 +1,19 @@
-BTSelector
+class Node {
+    tick(){
 
-BTSequence
+    }
+}
 
-BTCondition
-
-BTAction
-
+class Sequence extends Node{
+    constructor(children){
+        super();
+        this.children = children
+    }
+    tick(){
+        for (let child of this.children){
+            const status = child.tick
+            if(status !== 'SUCCESS') return status
+            return 'SUCCESS'
+        }
+    }
+}
